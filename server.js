dbcadf12518fc67788eb44a18fc7e8c904475b12
victor augusto adriano alves <vadriano@magnasistemas.com.br>
2023-12-13 14:35:49 -0300

    adicionando navbar com cards

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = 3000; 
 
-app.use(express.static(path.join(__dirname, 'public'))); // Define o diretório 'public' como conteúdo estático
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/play', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Envia o arquivo HTML para a rota raiz
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); 
 });
 
 app.listen(port, () => {
