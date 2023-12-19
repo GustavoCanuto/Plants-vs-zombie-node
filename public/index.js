e58dce94efc00b7928e3e427bba77a78f962e14d
Gustavo Henrique Canuto Cruz <131610213+GustavoCanuto@users.noreply.github.com>
2023-12-18 21:42:09 -0300

    teste comandos

--- public/index.js
+++ public/index.js
@@ -1,12 +1,19 @@
 const linhas = 5;
-const colunas = 8;
-const colunasPorLinha = 8; 
+const colunas = 10;
+const colunasPorLinha = 10;
 
 let tabuleiro = new Array(linhas);
 
 for (let i = 0; i < linhas; i++) {
     tabuleiro[i] = new Array(colunas).fill('-');
 }
+
+// Adicionando carrinhos de grama em posições específicas
+tabuleiro[0][0] = 'C';
+tabuleiro[1][0] = 'C';
+tabuleiro[2][0] = 'C';
+tabuleiro[3][0] = 'C';
+tabuleiro[4][0] = 'C';
 
 function mostrarTabuleiro() {
     const boardElement = document.getElementById('board');
@@ -15,12 +22,14 @@ function mostrarTabuleiro() {
     for (let i = 0; i < linhas; i++) {
         const rowElement = document.createElement('div');
         rowElement.classList.add('row');
+        rowElement.classList.add('linha' + (i + 1)); // Adicionando classe para identificar a linha
 
         for (let j = 0; j < colunas; j++) {
             if (j % colunasPorLinha === 0 && j !== 0) {
                 boardElement.appendChild(rowElement);
                 rowElement = document.createElement('div');
                 rowElement.classList.add('row');
+                rowElement.classList.add('linha' + (i + 1)); // Adicionando classe para identificar a linha
             }
 
             const cell = document.createElement('div');
@@ -30,6 +39,8 @@ function mostrarTabuleiro() {
                 cell.classList.add('plant');
             } else if (tabuleiro[i][j] === 'Z') {
                 cell.classList.add('zombie');
+            } else if (tabuleiro[i][j] === 'C') {
+                cell.classList.add('grass-cutter');
             }
 
             cell.textContent = tabuleiro[i][j];
@@ -41,8 +52,25 @@ function mostrarTabuleiro() {
 }
 
 // Atualizando algumas células para representar plantas e zumbis
-tabuleiro[0][0] = 'P';
-tabuleiro[0][7] = 'Z';
+tabuleiro[0][1] = 'P';
+tabuleiro[0][8] = 'Z';
 
-// Exibindo o tabuleiro inicializado
 mostrarTabuleiro();
+
+// // Limitando o movimento do zombie somente na linha 1
+// if (tabuleiro[0][8] === 'Z') {
+//     const zombieCell = document.querySelector('.linha1 .cell:nth-child(8)'); // Encontra a célula do zombie na linha 1
+
+//     // Verificando se há um carrinho de grama ('C') na mesma posição que o zombie
+//     if (tabuleiro[0][8] === 'C') {
+//         // Lógica para remover o zombie ou ações específicas ao ser morto pelo carrinho de grama
+//         tabuleiro[0][8] = '-'; // Remover o zombie da posição
+//     } else {
+//         // Lógica de movimento padrão do zombie
+//         zombieCell.textContent = '-';
+//         tabuleiro[0][7] = '-';
+//         tabuleiro[0][6] = 'Z';
+//     }
+
+//     mostrarTabuleiro(); // Atualizar o tabuleiro após o movimento
+// }
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000; // Escolha a porta que deseja utilizar
+const port = 3000; 
 
-app.use(express.static(path.join(__dirname, 'public'))); // Define o diretório 'public' como conteúdo estático
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/play', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html')); // Envia o arquivo HTML para a rota raiz
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); 
 });
 
 app.listen(port, () => {
