6b16eb7533431b130ee0eb0e8e08322c168f619c
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-01-05 18:01:13 -0300

    escolhendo personagem pela navBar

--- public/assets/js/comandos/mouse/mouseNavBar.js
+++ public/assets/js/comandos/mouse/mouseNavBar.js
@@ -1,6 +1,9 @@
+import {Personagens, personagens} from "../../personagens.js";
+
 var cellNavBarZombie = document.querySelectorAll('.navbar-zombie .card');
 var cellNavBarZombieAtual = document.querySelector('.cardtombstone');
 var cursorNavBarZombie = document.getElementById('cursor-navBar-zombie');
+var prevPersonagemImg = document.getElementById('prevPersonagem');
 
 cellNavBarZombie.forEach(function (cell) {
 
@@ -9,7 +12,21 @@ cellNavBarZombie.forEach(function (cell) {
         cellNavBarZombieAtual.classList.remove('seletorNavBar');
         cellNavBarZombieAtual = cell;
 
+    
+
         if (!cellNavBarZombieAtual.classList.contains('pontuacao-zombie')) {
+
+            const personagemNome = cell.getAttribute('data-personagem');
+
+            if (personagens[personagemNome]) {
+          
+                // Atualize a imagem do prevPersonagemImg
+                prevPersonagemImg.src = personagens[personagemNome].imagePath;
+
+               
+            }
+
+            
             cell.classList.add('seletorNavBar');
            
             cell.appendChild(cursorNavBarZombie);
