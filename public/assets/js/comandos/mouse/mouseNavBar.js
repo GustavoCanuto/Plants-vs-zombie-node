a6a99ca967c054a48c3b3831dec139a885833655
GustavoCanuto <gustavo-canuto@hotmail.com>
2024-01-05 20:34:44 -0300

    click e percorrer navBar com scroll

--- public/assets/js/comandos/mouse/mouseNavBar.js
+++ public/assets/js/comandos/mouse/mouseNavBar.js
@@ -1,5 +1,7 @@
-import {Personagens, personagens} from "../../personagens.js";
+import {personagens} from "../../personagens.js";
+import {clickCelula} from './clickCelula.js';
 
+var navbarZombie = document.querySelector('.navbar-zombie');
 var cellNavBarZombie = document.querySelectorAll('.navbar-zombie .card');
 var cellNavBarZombieAtual = document.querySelector('.cardtombstone');
 var cursorNavBarZombie = document.getElementById('cursor-navBar-zombie');
@@ -35,3 +37,35 @@ cellNavBarZombie.forEach(function (cell) {
 
     });
 });
+
+document.addEventListener('wheel', function (event) {
+
+    // Verifique a direção do scroll
+    var direction = event.deltaY > 0 ? 1 : -1;
+
+    // Encontre o índice da célula atual
+    var currentIndex = Array.from(cellNavBarZombie).indexOf(cellNavBarZombieAtual);
+
+    // Calcule o próximo índice com base na direção do scroll
+    var nextIndex = currentIndex + direction;
+
+    // Garanta que o próximo índice está dentro dos limites
+    nextIndex = Math.max(0, Math.min(cellNavBarZombie.length - 1, nextIndex));
+
+    // Mova a div seletorTabuleiro para a nova célula
+    cellNavBarZombieAtual.classList.remove('seletorNavBar');
+    cellNavBarZombieAtual = cellNavBarZombie[nextIndex];
+
+    const personagemNome = cellNavBarZombieAtual.getAttribute('data-personagem');
+
+    if (personagens[personagemNome]) {
+        // Atualize a imagem do prevPersonagemImg
+        prevPersonagemImg.src = personagens[personagemNome].imagePath;
+    }
+
+    cellNavBarZombieAtual.classList.add('seletorNavBar');
+    cellNavBarZombieAtual.appendChild(cursorNavBarZombie);
+});
+
+
+clickCelula();
