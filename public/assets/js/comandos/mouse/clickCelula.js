5304bfd041b12fd55fc6a1fd2c6f5277131f3adb
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-01-06 00:40:55 -0300

    comando navBar por gamePad, teclado e mouse

--- public/assets/js/comandos/mouse/clickCelula.js
+++ public/assets/js/comandos/mouse/clickCelula.js
@@ -1,23 +1,14 @@
+import {dropPersonagem} from '../comandosNavBar.js';
+
 export function clickCelula(){
 
-cells.forEach(function (cell) {
+cells.forEach(function(cell) {
 
-    
     if (!cell.classList.contains('grass-cutter')) {
 
     cell.addEventListener('click', function () {
         
-    if (!cell.classList.contains('ocupado')) {
-
-        const elemento = document.createElement('div');
-        elemento.classList.add("personagem");
-        var imgPersonagem = document.createElement('img');
-        imgPersonagem.src = imgPreviaPersonagem.src;
-        elemento.appendChild(imgPersonagem);
-        cell.appendChild(elemento);
-        elemento.closest('.cell').classList.add("ocupado");
-
-        }
+        dropPersonagem(cell, imgPreviaPersonagem);
     }
     );
     }
