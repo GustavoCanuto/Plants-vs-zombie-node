76b9bed09ab6b6f573fed0d42915b795ed23845c
GustavoCanuto <gustavo-canuto@hotmail.com>
2024-01-07 19:54:24 -0300

    seletor navBar somente para lado jogador usa mouse

--- public/assets/js/comandos/comandosNavBar.js
+++ public/assets/js/comandos/comandosNavBar.js
@@ -1,33 +1,49 @@
 import {personagens} from "../personagens.js";
 
+//navBar Planta
+export var cellNavBarPlanta = document.querySelectorAll('.navbar-planta .card');
+export var cellNavBarPlantaAtual = document.querySelector('.sunflower');
+export var cursorNavBarPlanta = document.getElementById('cursor-navBar-planta');
+
+
+//navBar zombie
 export var cellNavBarZombie = document.querySelectorAll('.navbar-zombie .card');
 export var cellNavBarZombieAtual = document.querySelector('.cardtombstone');
 export var cursorNavBarZombie = document.getElementById('cursor-navBar-zombie');
+
+
+//geral
+export var cellNavBar        = [cellNavBarPlanta,cellNavBarZombie]
+export var cellNavBarAtual   = [cellNavBarPlantaAtual, cellNavBarZombieAtual]
+export var cursorNavBar      = [cursorNavBarPlanta, cursorNavBarZombie]
 export var prevPersonagemImg = document.getElementById('prevPersonagem');
 
-export function moveNavBar(direction){
 
-    var currentIndex = Array.from(cellNavBarZombie).indexOf(cellNavBarZombieAtual);
+export function moveNavBar(direction, lado){
+
+    var currentIndex = Array.from(cellNavBar[lado]).indexOf(cellNavBarAtual[lado]);
 
     // Calcule o próximo índice com base na direção do scroll
     var nextIndex = currentIndex + direction;
 
     // Garanta que o próximo índice está dentro dos limites
-    nextIndex = Math.max(0, Math.min(cellNavBarZombie.length - 1, nextIndex));
+    nextIndex = Math.max(0, Math.min(cellNavBar[lado].length - 1, nextIndex));
 
-    // Mova a div seletorTabuleiroAzul para a nova célula
-    cellNavBarZombieAtual.classList.remove('seletorNavBarAzul');
-    cellNavBarZombieAtual = cellNavBarZombie[nextIndex];
+    // Mova a div seletorTabuleiropara a nova célula
+    const classeNavBar =  lado == 0 ? 'seletorNavBarAmarelo' : 'seletorNavBarAzul'
 
-    const personagemNome = cellNavBarZombieAtual.getAttribute('data-personagem');
+    cellNavBarAtual[lado].classList.remove(`${classeNavBar}`);
+    cellNavBarAtual[lado] = cellNavBar[lado][nextIndex];
+ 
+    const personagemNome = cellNavBarAtual[lado].getAttribute('data-personagem');
 
     if (personagens[personagemNome]) {
         // Atualize a imagem do prevPersonagemImg
         prevPersonagemImg.src = personagens[personagemNome].imagePath;
     }
 
-    cellNavBarZombieAtual.classList.add('seletorNavBarAzul');
-    cellNavBarZombieAtual.appendChild(cursorNavBarZombie);
+    cellNavBarAtual[lado].classList.add(`${classeNavBar}`);
+    cellNavBarAtual[lado].appendChild(cursorNavBar[lado]);
 
 }
 
@@ -45,13 +61,15 @@ export function dropPersonagem(cell, imgPreviaPersonagem){
     }
 }
 
-export function criarPreviaPersonagem(cell){
+export function criarPreviaPersonagem(cell, lado){
 
+    const classeNavBar =  lado == 0 ? 'seletorNavBarAmarelo' : 'seletorNavBarAzul'
+    const divNavBar = lado == 0 ? 'pontuacao-planta' : 'pontuacao-zombie'
     // Mova a div seletorTabuleiroAzul para a célula atual
-    cellNavBarZombieAtual.classList.remove('seletorNavBarAzul');
-    cellNavBarZombieAtual = cell;
+    cellNavBarAtual[lado].classList.remove(`${classeNavBar}`);
+    cellNavBarAtual[lado] = cell;
 
-    if (!cellNavBarZombieAtual.classList.contains('pontuacao-zombie')) {
+    if (!cellNavBarAtual[lado].classList.contains(`${divNavBar}`)) {
 
         const personagemNome = cell.getAttribute('data-personagem');
 
@@ -60,8 +78,8 @@ export function criarPreviaPersonagem(cell){
             prevPersonagemImg.src = personagens[personagemNome].imagePath;
         }
         
-        cell.classList.add('seletorNavBarAzul'); 
-        cell.appendChild(cursorNavBarZombie);
+        cell.classList.add(`${classeNavBar}`); 
+        cell.appendChild(cursorNavBar[lado]);
 
     }
 }
