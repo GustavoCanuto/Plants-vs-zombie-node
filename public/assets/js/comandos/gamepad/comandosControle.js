c1896b0a38f48eaf58030e98ecb7ec27dfa26390
GustavoCanuto <gustavo-canuto@hotmail.com>
2024-01-08 18:43:28 -0300

    movimentos simultaneo no mesmo teclado

--- public/assets/js/comandos/gamepad/comandosControle.js
+++ public/assets/js/comandos/gamepad/comandosControle.js
@@ -1,68 +1,91 @@
 import { moveContent, moveCards } from '../teclado/teclado.js';
 
 
-const tempoMinimoPressionado = 200;
-//const tempoMinimoPressionado2 = 150;
-var esperaButtons = true;
-var esperaButtonsDuplo = true;
-var timoutButton;
+// const tempoMinimoPressionado = 200;
+// //const tempoMinimoPressionado2 = 150;
+// var esperaButtons = true;
+// var esperaButtonsDuplo = true;
+// var timoutButton;
 
 export function botoesGamepad(buttons, lado) {
 
-    //var chaveLado = Object.keys(celulaAtual[lado]);
+    if (lado == 0) jogadorPlantaComandos.atualizarComandos(buttons, lado)
+    if (lado == 1) jogadorZombieComandos.atualizarComandos(buttons, lado)
+
+}
+
+class jogadorComandosGamePad {
+
+    constructor() {
+        this.tempoMinimoPressionado = 200;
+        this.esperaButtons = true;
+        this.esperaButtonsDuplo = true;
+        this.timoutButton;
+        this.buttons;
+    }
+
 
     // Função para verificar a duração da pressão e chamar a função correspondente
-    function verificarBotaoPressionado(indice, funcao) {
+    verificarBotaoPressionado(indice, funcao) {
 
         // Se o botão estiver sendo pressionado
-        if (buttons[indice].pressed) {
+        if (this.buttons[indice].pressed) {
 
-            if (esperaButtons) {
+            if (this.esperaButtons) {
 
-                timoutButton = setTimeout(() => {
+                this.timoutButton = setTimeout(() => {
                     funcao();
-                    esperaButtons = true;
-                }, tempoMinimoPressionado);
+                    this.esperaButtons = true;
+                }, this.tempoMinimoPressionado);
 
-                esperaButtons = false;
+                this.esperaButtons = false;
 
             }
         }
     }
 
     // Função para verificar a duração da pressão e chamar a função correspondente
-    function verificarBotaoPressionadoDuplo(indice1, indice2, funcao) {
+    verificarBotaoPressionadoDuplo(indice1, indice2, funcao) {
 
         // Se o botão estiver sendo pressionado
-        if (buttons[indice1].pressed && buttons[indice2].pressed) {
+        if (this.buttons[indice1].pressed && this.buttons[indice2].pressed) {
 
-            if (esperaButtonsDuplo) {
-                clearTimeout(timoutButton);
+            if (this.esperaButtonsDuplo) {
+                clearTimeout(this.timoutButton);
 
                 setTimeout(() => {
                     funcao();
-                    esperaButtonsDuplo = true;
-                    esperaButtons = true;
-                }, tempoMinimoPressionado);
+                    this.esperaButtonsDuplo = true;
+                    this.esperaButtons = true;
+                }, this.tempoMinimoPressionado);
 
-                esperaButtonsDuplo = false;
+                this.esperaButtonsDuplo = false;
 
             }
         }
+    }
+
+    atualizarComandos(buttons, lado) {
+
+        this.buttons = buttons;
+
+        // Exemplo de como usar a função para os botões específicos
+        this.verificarBotaoPressionado(15, () => moveContent('arrowright', lado));
+        this.verificarBotaoPressionado(14, () => moveContent('arrowleft', lado));
+        this.verificarBotaoPressionado(13, () => moveContent('arrowdown', lado));
+        this.verificarBotaoPressionado(12, () => moveContent('arrowup', lado));
+
+        this.verificarBotaoPressionado(0, () => moveCards(' ', lado));
+        this.verificarBotaoPressionado(4, () => moveCards('1', lado));
+        this.verificarBotaoPressionado(5, () => moveCards('2', lado));
+
+        this.verificarBotaoPressionadoDuplo(12, 14, () => moveContent('arrowupleft', lado));
+        this.verificarBotaoPressionadoDuplo(12, 15, () => moveContent('arrowupright', lado));
+        this.verificarBotaoPressionadoDuplo(13, 14, () => moveContent('arrowdownleft', lado));
+        this.verificarBotaoPressionadoDuplo(13, 15, () => moveContent('arrowdownright', lado));
+    }
+
 }
 
-    // Exemplo de como usar a função para os botões específicos
-    verificarBotaoPressionado(15, () => moveContent('arrowright', lado));
-    verificarBotaoPressionado(14, () => moveContent('arrowleft', lado));
-    verificarBotaoPressionado(13, () => moveContent('arrowdown', lado));
-    verificarBotaoPressionado(12, () => moveContent('arrowup', lado));
-
-    verificarBotaoPressionado(0, () => moveCards(' ', lado));
-    verificarBotaoPressionado(4, () => moveCards('1', lado));
-    verificarBotaoPressionado(5, () => moveCards('2', lado));
-
-    verificarBotaoPressionadoDuplo(12, 14, () => moveContent('arrowupleft', lado));
-    verificarBotaoPressionadoDuplo(12, 15, () => moveContent('arrowupright', lado));
-    verificarBotaoPressionadoDuplo(13, 14, () => moveContent('arrowdownleft', lado));
-    verificarBotaoPressionadoDuplo(13, 15, () => moveContent('arrowdownright', lado));
-}
+export var jogadorPlantaComandos = new jogadorComandosGamePad();
+export var jogadorZombieComandos = new jogadorComandosGamePad();
