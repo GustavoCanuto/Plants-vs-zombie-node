841448f451234ce549f0c07bd7b1ddd0cda90a56
raacodes <poleguin@gmail.com>
2024-01-10 08:18:16 -0300

    ajustando arquivo animacaoCartas.js

--- public/assets/js/animacaoCartas.js
+++ public/assets/js/animacaoCartas.js
@@ -32,6 +32,19 @@ class AnimacaoCartas {
         }
 
         const nomeClasse = imgSrc.split('/').pop().split('.')[0];
+
+        // Verifica se a classe pertence ao conjunto de plantas
+        const isPlanta = ['sunflower', 'peashooter', 'showpea', 'repeater', 'wallnut', 'cherrybomb', 'potatomine'].includes(nomeClasse);
+
+        if (isPlanta) {
+            this.criarAnimacaoPlanta(cellElement, nomeClasse, this.framesPorClasse);
+        } else {
+            this.criarAnimacaoZombie(cellElement, nomeClasse);
+        }
+    }
+
+
+    static criarAnimacaoPlanta(cellElement, nomeClasse, framesPorClasse) {
         const numberOfFrames = this.framesPorClasse[nomeClasse] || 0;
         const frames = this.carregarFrames(nomeClasse, numberOfFrames);
 
@@ -41,46 +54,79 @@ class AnimacaoCartas {
         }
 
         const elemento = document.createElement('div');
-        elemento.classList.add('personagem');
+        elemento.classList.add('personagemPlanta');
         const gifElement = document.createElement('img');
         elemento.appendChild(gifElement);
-        var tabuleiro = document.querySelector('.board');
-        tabuleiro.appendChild(elemento);
-
-        const posicaoLeft = cellElement.offsetLeft ;
-        const posicaoTop = cellElement.offsetTop-50;
-
-   
-        //alert(posicaoLeft)
-        elemento.style.position = 'absolute'
-        elemento.style.top = `${posicaoTop}px`;
-        elemento.style.left =  `${posicaoLeft}px`;
-        const fatorEscala = 0.8;
-
-        let positionLeft = posicaoLeft;
-
-        function moveElement() {
-         
-          elemento.style.left = positionLeft + 'px';
-          positionLeft += 10;
-        }
-        
-        setInterval(moveElement, 100); 
-        elemento.style.transform = `scale(${fatorEscala})`;
+        cellElement.appendChild(elemento);
 
         this.iniciarAnimacao(frames, gifElement);
     }
 
-    static carregarFrames(nomeClasse, numberOfFrames) {
-        const frames = [];
-
-        for (let i = 1; i <= numberOfFrames; i++) {
-            const imgFrame = new Image();
-            imgFrame.src = `../assets/img/frames/${nomeClasse}/frame-${i}.gif`;
-            frames.push(imgFrame);
+    static criarAnimacaoZombie(cellElement, nomeClasse) {
+        const elemento = document.createElement('div');
+        elemento.classList.add('personagemZombie');
+        const gifElement = document.createElement('img');
+        elemento.appendChild(gifElement);
+        let tabuleiro = document.querySelector('.board');
+        tabuleiro.appendChild(elemento);
+    
+        function atualizarPosicao(posLeft, posTop, cellWidth, cellHeight) {
+            elemento.style.position = 'absolute';
+            elemento.style.width = `${cellWidth}px`;
+            elemento.style.height = `${cellHeight}px`;
+            elemento.style.top = `${(posTop / window.innerHeight) * 100}vh`;
+            elemento.style.left = `${(posLeft / window.innerWidth) * 100}vw`;
+    
+            // Ajuste do tamanho da imagem
+            gifElement.style.maxWidth = `${cellWidth}px`;
+            gifElement.style.maxHeight = `${cellHeight}px`;
         }
-        return frames;
+    
+        const atualizarPosicaoInicial = () => {
+            const cellWidth = cellElement.offsetWidth;
+            const cellHeight = cellElement.offsetHeight;
+            const posicaoLeft = cellElement.offsetLeft;
+            const posicaoTop = cellElement.offsetTop;
+    
+            atualizarPosicao(posicaoLeft, posicaoTop, cellWidth, cellHeight);
+        };
+    
+        atualizarPosicaoInicial(); // Chamar inicialmente ao criar o elemento
+    
+        window.addEventListener('resize', () => {
+            const newCellWidth = cellElement.offsetWidth;
+            const newCellHeight = cellElement.offsetHeight;
+            const newPosicaoLeft = cellElement.offsetLeft;
+            const newPosicaoTop = cellElement.offsetTop;
+    
+            atualizarPosicao(newPosicaoLeft, newPosicaoTop, newCellWidth, newCellHeight); // Recalcular posição e tamanho ao redimensionar a tela
+        });
+    
+        const numberOfFrames = this.framesPorClasse[nomeClasse] || 0;
+        const frames = this.carregarFrames(nomeClasse, numberOfFrames);
+    
+        if (frames.length === 0) {
+            console.error('Não foi possível carregar os frames da animação.');
+            return;
+        }
+    
+        gifElement.style.transform = `scale(1.5)`;
+    
+        // Lógica de movimento para a esquerda
+        //const fatorEscala = 1.5;
+        let positionLeft = elemento.offsetLeft;
+    
+        function moveElement() {
+            elemento.style.left = positionLeft + 'px';
+            positionLeft -= 3;
+        }
+    
+        setInterval(moveElement, 100);
+        
+        this.iniciarAnimacao(frames, gifElement);
     }
+    
+    
 
     static iniciarAnimacao(frames, gifElement) {
     let frameIndex = 0;
@@ -96,6 +142,18 @@ class AnimacaoCartas {
         }
     }, frameDuration);
   }
+
+  static carregarFrames(nomeClasse, numberOfFrames) {
+    const frames = [];
+
+    for (let i = 1; i <= numberOfFrames; i++) {
+        const imgFrame = new Image();
+        imgFrame.src = `../assets/img/frames/${nomeClasse}/frame-${i}.gif`;
+        frames.push(imgFrame);
+    }
+
+    return frames;
+}
 }
 
 export default AnimacaoCartas;
