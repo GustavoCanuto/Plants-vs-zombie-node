d76dc4a194a08020b7d2dd31fbd630196ce8ac4c
raacodes <poleguin@gmail.com>
2024-01-10 14:05:43 -0300

    refatorando e organizando animacaoCartas.js

--- public/assets/js/animacaoCartas.js
+++ public/assets/js/animacaoCartas.js
@@ -32,74 +32,112 @@ class AnimacaoCartas {
         }
 
         const nomeClasse = imgSrc.split('/').pop().split('.')[0];
-        const numberOfFrames = this.framesPorClasse[nomeClasse] || 0;
-        const frames = this.carregarFrames(nomeClasse, numberOfFrames);
 
-        if (frames.length === 0) {
-            console.error('Não foi possível carregar os frames da animação.');
-            return;
-        }
+        const isPlanta = ['sunflower', 'peashooter', 'showpea', 'repeater', 'wallnut', 'cherrybomb', 'potatomine'].includes(nomeClasse);
 
-        const elemento = document.createElement('div');
-        elemento.classList.add('personagem');
-        const gifElement = document.createElement('img');
-        elemento.style.width = '20%'
-        elemento.appendChild(gifElement);
-        var tabuleiro = document.querySelector('.board');
-        tabuleiro.appendChild(elemento);
-
-        
-        gifElement.style.width = '100%'
-
-      //  alert(elemento.offsetHeight)
-      //  alert(elemento.getBoundingClientRect().height)
-        const posicaoLeft = (cellElement.offsetLeft / tabuleiro.offsetWidth) * 100;
-        let posicaoTop = (((cellElement.offsetTop)- 30) / tabuleiro.offsetHeight) * 100;
- 
-        let posicaoBottom = 72 - posicaoTop ;
-        elemento.style.position = 'absolute';
-        elemento.style.bottom = `${posicaoBottom}%`;
-        elemento.style.left = `${posicaoLeft}%`;
-    
-        const fatorEscala = 0.8;
-        let positionLeft = posicaoLeft;
-    
-        function moveElement() {
-            elemento.style.left = positionLeft + '%';
-            positionLeft -= 1; // Ajuste a velocidade conforme necessário
-        }
-        
-        setInterval(moveElement, 100); 
-        elemento.style.transform = `scale(${fatorEscala})`;
-
-        this.iniciarAnimacao(frames, gifElement);
-    }
-
-    static carregarFrames(nomeClasse, numberOfFrames) {
-        const frames = [];
-
-        for (let i = 1; i <= numberOfFrames; i++) {
-            const imgFrame = new Image();
-            imgFrame.src = `../assets/img/frames/${nomeClasse}/frame-${i}.gif`;
-            frames.push(imgFrame);
-        }
-        return frames;
-    }
-
-    static iniciarAnimacao(frames, gifElement) {
-    let frameIndex = 0;
-    const frameDuration = 50;
-
-    const animationInterval = setInterval(() => {
-        if (gifElement) {
-            gifElement.src = frames[frameIndex].src;
-            frameIndex = (frameIndex + 1) % frames.length;
+        if (isPlanta) {
+            this.criarAnimacaoPlanta(cellElement, nomeClasse, this.framesPorClasse);
         } else {
-            clearInterval(animationInterval);
-            console.error('Elemento gifElement não encontrado.');
+            this.criarAnimacaoZombie(cellElement, nomeClasse);
         }
-    }, frameDuration);
-  }
-}
+    }
+
+
+        static criarAnimacaoPlanta(cellElement, nomeClasse, framesPorClasse){
+            const numberOfFrames = this.framesPorClasse[nomeClasse] || 0;
+            const frames = this.carregarFrames(nomeClasse, numberOfFrames);
+
+            if (frames.length === 0) {
+                console.error('Não foi possível carregar os frames da animação.');
+                return;
+            }
+
+            const elemento = document.createElement('div');
+            elemento.classList.add('personagemPlanta');
+            const gifElement = document.createElement('img');
+            elemento.appendChild(gifElement);
+            cellElement.appendChild(elemento);
+
+            this.iniciarAnimacao(frames, gifElement);
+
+        }
+
+        static criarAnimacaoZombie(cellElement, nomeClasse){
+            const elemento = document.createElement('div');
+            elemento.classList.add('personagem');
+            const gifElement = document.createElement('img');
+            elemento.style.width = '15%'
+            elemento.appendChild(gifElement);
+            let tabuleiro = document.querySelector('.board');
+
+            gifElement.style.width = '100%'
+
+     
+            tabuleiro.appendChild(elemento)
+
+            const posicaoLeft = (cellElement.offsetLeft / tabuleiro.offsetWidth) * 100;
+            let posicaoTop = (((cellElement.offsetTop) - (elemento.offsetHeight+20)) / tabuleiro.offsetHeight) * 100;
+
+            let posicaoBottom = 72 - posicaoTop ;
+            elemento.style.position = 'absolute';
+        
+            elemento.style.bottom = `${posicaoBottom}%`;
+            elemento.style.left = `${posicaoLeft}%`;
+
+            const numberOfFrames = this.framesPorClasse[nomeClasse] || 0;
+            const frames = this.carregarFrames(nomeClasse, numberOfFrames);
+
+            if (frames.length === 0) {
+                console.error('Não foi possível carregar os frames da animação.');
+                return;
+            }
+            
+            let positionLeft = posicaoLeft;
+        
+            function moveElement() {
+                if(positionLeft > -5){
+                elemento.style.left = positionLeft + '%';
+            }
+            
+                positionLeft -= 1; // Ajuste a velocidade conforme necessário
+            }
+            
+            setInterval(moveElement, 100); 
+            this.iniciarAnimacao(frames, gifElement);
+
+        
+            elemento.classList.remove('adicionado')
+
+    }
+
+        static iniciarAnimacao(frames, gifElement) {
+            let frameIndex = 0;
+            const frameDuration = 50;
+        
+            const animationInterval = setInterval(() => {
+                if (gifElement) {
+                    gifElement.src = frames[frameIndex].src;
+                    frameIndex = (frameIndex + 1) % frames.length;
+                } else {
+                    clearInterval(animationInterval);
+                    console.error('Elemento gifElement não encontrado.');
+                }
+            }, frameDuration);
+          }
+        
+        
+          static carregarFrames(nomeClasse, numberOfFrames) {
+            const frames = [];
+        
+            for (let i = 1; i <= numberOfFrames; i++) {
+                const imgFrame = new Image();
+                imgFrame.src = `../assets/img/frames/${nomeClasse}/frame-${i}.gif`;
+                frames.push(imgFrame);
+            }
+        
+            return frames;
+        }
+    }
+        
 
 export default AnimacaoCartas;
