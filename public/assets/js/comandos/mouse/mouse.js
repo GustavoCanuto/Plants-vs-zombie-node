fee0330e39cd3ba5b43a2a69b802992857c6d586
raacodes <poleguin@gmail.com>
2024-01-12 17:01:13 -0300

    ajustando código para corrigir bugs na sincronização do socket.io

--- public/assets/js/comandos/mouse/mouse.js
+++ public/assets/js/comandos/mouse/mouse.js
@@ -33,7 +33,7 @@ var main = document.querySelector('main');
 var tabuleiroID = document.getElementById('tabuleiroID');
 var cells = document.querySelectorAll('.cell');
 
-var LadoQueUsaMouse = 1;
+var LadoQueUsaMouse = 0;
 
 var cursorTabuleiro  =  [{planta:cursorTabuleiroAmarelo}, {zombie:cursorTabuleiroAzul}]
 var seletorTabuleiro =  [{planta:seletorTabuleiroAmarelo},{zombie:seletorTabuleiroAzul}]
