0edc80586f4873aede5d425fa0e94c3a9bdc966d
Gustavo Canuto <gcanuto.dev@gmail.com>
2024-01-11 21:30:13 -0300

    merge pontuacao com colisao

--- public/assets/js/personagens.js
+++ public/assets/js/personagens.js
@@ -1,6 +1,8 @@
 export class Personagens {
-    constructor(imagePath) {
+    constructor(imagePath, tempoRecarga) {
         this.imagePath = imagePath;
+        this.tempoRecarga = tempoRecarga;
+        this.recarregado = true;
     }
 
     renderImage() {
@@ -13,21 +15,20 @@ export class Personagens {
 
 }
 
-
-const sunflower = new Personagens('../../assets/img/personagens/plants/sunflower.png');
-const peashooter = new Personagens('../../assets/img/personagens/plants/peashooter.png');
-const showpea = new Personagens('../../assets/img/personagens/plants/showpea.png');
-const repeater = new Personagens('../../assets/img/personagens/plants/repeater.png');
-const wallnut = new Personagens('../../assets/img/personagens/plants/wallnut.png');
-const cherrybomb = new Personagens('../../assets/img/personagens/plants/cherrybomb.png');
-const potatomine = new Personagens('../../assets/img/personagens/plants/potatomine.png');
-const cardtombstone = new Personagens('../../assets/img/personagens/zombies/cardtombstone.png');
-const zombie = new Personagens('../../assets/img/personagens/zombies/zombie.png');
-const conehead = new Personagens('../../assets/img/personagens/zombies/conehead.png');
-const buckethead = new Personagens('../../assets/img/personagens/zombies/buckethead.png');
-const flagzombie = new Personagens('../../assets/img/personagens/zombies/flagzombie.png');
-const football = new Personagens('../../assets/img/personagens/zombies/football.png');
-const screendoor = new Personagens('../../assets/img/personagens/zombies/screendoor.png');
+const sunflower = new Personagens('../../assets/img/personagens/plants/sunflower.png', 7);
+const peashooter = new Personagens('../../assets/img/personagens/plants/peashooter.png', 2);
+const showpea = new Personagens('../../assets/img/personagens/plants/showpea.png', 2);
+const repeater = new Personagens('../../assets/img/personagens/plants/repeater.png', 2);
+const wallnut = new Personagens('../../assets/img/personagens/plants/wallnut.png', 30);
+const cherrybomb = new Personagens('../../assets/img/personagens/plants/cherrybomb.png',20);
+const potatomine = new Personagens('../../assets/img/personagens/plants/potatomine.png',10);
+const cardtombstone = new Personagens('../../assets/img/personagens/zombies/cardtombstone.png', 7);
+const zombie = new Personagens('../../assets/img/personagens/zombies/zombie.png', 7);
+const conehead = new Personagens('../../assets/img/personagens/zombies/conehead.png', 10);
+const buckethead = new Personagens('../../assets/img/personagens/zombies/buckethead.png', 10);
+const flagzombie = new Personagens('../../assets/img/personagens/zombies/flagzombie.png', 30);
+const football = new Personagens('../../assets/img/personagens/zombies/football.png', 10);
+const screendoor = new Personagens('../../assets/img/personagens/zombies/screendoor.png', 5);
 
 export const personagens = {
     sunflower: sunflower,
---
+++ public/assets/js/pontuacao.js
@@ -0,0 +1,115 @@
+var pontuacaoLado = [50, 50];
+var pontoPlantas = document.getElementById("pontosPlanta");
+var pontoZombies = document.getElementById("pontosZombie");
+var pontosLado = [pontoPlantas, pontoZombies]
+var board = document.querySelector('.board')
+
+function cairPontos(lado) {
+
+  let posicaoTop = -7
+  let topLimite = Math.random() < 0.75 ? 83 : 51;
+  let pontuacao;
+  let posicaoLeft;
+
+  if (lado == 0) {
+    posicaoLeft = Math.floor(Math.random() * (48 - 18 + 1)) + 18;
+    pontuacao = criarSol();
+    pontuacao.style.left = `${posicaoLeft}%`
+  }else{
+    posicaoLeft = Math.floor(Math.random() * (95 - 68 + 1)) + 68;
+    pontuacao = criarCerebro();
+    pontuacao.style.left = `${posicaoLeft}%`
+  }
+
+  pontuacao.style.top = "-7%"
+  let setIntervalSolCaindo = setInterval(() => {
+
+    var chaveLado = Object.keys(celulaAtual[lado]);
+    if (verificaColisaoCelular(pontuacao, celulaAtual[lado][chaveLado])) {
+      setTimeout(() => {
+        pontuacao.style.transition = 'all 0.5s ease'
+        pontuacao.style.top = lado == 0 ? "-21%": "-21%";
+        pontuacao.style.left = lado == 0 ? "6%": "100%";
+        setTimeout(() => {
+          pontuacao.style.opacity = 0;
+          setTimeout(() => {
+            board.removeChild(pontuacao);
+          }, 600);
+        }, 400);
+        pontuacaoLado[lado] += 50;
+        pontosLado[lado].textContent = pontuacaoLado[lado];
+      }, 50);
+
+      clearInterval(setIntervalSolCaindo)
+    }
+
+    posicaoTop++;
+
+    //console.log(posicaoTop)
+    if (posicaoTop < topLimite) {
+      pontuacao.style.top = `${posicaoTop}%`
+    } else {
+      setTimeout(() => {
+        pontuacao.style.opacity = 0.7;
+      }, 2000);
+      setTimeout(() => {
+        if (board.contains(pontuacao)) {
+          board.removeChild(pontuacao);
+        }
+        clearInterval(setIntervalSolCaindo)
+      }, 6000);
+
+
+    }
+
+  }, 150);
+}
+
+
+function criarSol() {
+  const solPontuacao = document.createElement("img");
+  solPontuacao.src = "./assets/img/pontuacao/Sun.gif";
+  solPontuacao.alt = "sol pontuacao";
+  solPontuacao.id = 'solPontuacao'
+
+  board.appendChild(solPontuacao)
+
+  return solPontuacao;
+}
+function criarCerebro() {
+  const cerebroPontuacao = document.createElement("img");
+  cerebroPontuacao.src = "./assets/img/pontuacao/cerebroPontos.png";
+  cerebroPontuacao.alt = "cerebro pontuacao";
+  cerebroPontuacao.id = 'cerebroPontuacao'
+
+  board.appendChild(cerebroPontuacao)
+
+  return cerebroPontuacao;
+}
+
+setInterval(function () {
+  cairPontos(0);
+}, 5000);
+
+setInterval(function () {
+  cairPontos(1);
+}, 6000);
+
+function verificaColisaoCelular(elementoA, elementoB) {
+  const aEsquerda = elementoA.offsetLeft;
+  const aTopo = elementoA.offsetTop;
+  const aDireita = aEsquerda + elementoA.offsetWidth;
+  const aBaixo = aTopo + elementoA.offsetHeight;
+
+  const bEsquerda = elementoB.offsetLeft;
+  const bTopo = elementoB.offsetTop;
+  const bDireita = bEsquerda + elementoB.offsetWidth;
+  const bBaixo = bTopo + elementoB.offsetHeight;
+
+  return (
+    aEsquerda < bDireita &&
+    aDireita > bEsquerda &&
+    aTopo < bBaixo &&
+    aBaixo > bTopo
+  );
+}
