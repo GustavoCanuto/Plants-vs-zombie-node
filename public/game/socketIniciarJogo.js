fed667c5fb2ad3a1a0b5f2bb85b32fec8da26060
victor-dev <victor-dev@pop-os.magna>
2024-01-18 14:15:52 -0300

    preparando branch para merge

--- public/game/socketIniciarJogo.js
+++
@@ -1,6 +0,0 @@
-socket2.on('iniciarJogo', () => {
-    console.log("iniciar Jogo")
-    LadoQueUsaMouse = ladoJogador;
-    chaveMouse = Object.keys(celulaAtual[ladoJogador]);
-    cursorTabuleiroMouse = ladoJogador == 0 ? cursorTabuleiroAmarelo: cursorTabuleiroAzul;
-});
