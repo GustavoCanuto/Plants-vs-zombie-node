fa0b7996832bcf799f3bdd1e9cfb4b1feb2ce40b
victor augusto adriano alves <vadriano@magnasistemas.com.br>
2024-01-19 16:35:48 -0300

    adicionando tiro nas plantas

--- public/assets/js/animacaoCartas.js
+++ public/assets/js/animacaoCartas.js
@@ -175,6 +175,7 @@ class AnimacaoCartas {
         elemento.closest('.cell').classList.add('ocupado')
 
         this.iniciarAnimacao(frames, gifElement);
+        this.iniciarAnimacaoTiro(cellElement, nomeClasse);
     }
     static criarAnimacaoZombie(cellElement, nomeClasse) {
         const elemento = document.createElement('div');
@@ -256,11 +257,55 @@ class AnimacaoCartas {
         }
         
         let intervaloMovimentoZumbi = setInterval(moveElement, 100);
-
         this.iniciarAnimacao(frames, gifElement);
+        
 
         elemento.classList.remove('adicionado');
     }
+
+    static iniciarAnimacaoTiro(cellElement, nomeClasse) {
+        const tiroElement = document.createElement('img');
+        let caminhoImagem;
+        let numeroTiros = 1;
+    
+        if (nomeClasse === 'peashooter') {
+            caminhoImagem = '../assets/img/peashooter_tiro.gif';
+        } else if (nomeClasse === 'showpea') {
+            caminhoImagem = '../assets/img/peashooter_tiro_gelo.gif';
+        } else if (nomeClasse === 'repeater') {
+            caminhoImagem = '../assets/img/peashooter_tiro_2.gif';
+            numeroTiros = 2; 
+        }
+    
+        if (caminhoImagem) {
+            for (let i = 0; i < numeroTiros; i++) {
+                setTimeout(() => {
+                    const tiroElementClone = tiroElement.cloneNode();
+                    tiroElementClone.src = caminhoImagem;
+                    tiroElementClone.classList.add('tiro');
+                    cellElement.appendChild(tiroElementClone);
+    
+                    let posicaoLeft = 20;
+                    const intervaloTiro = setInterval(() => {
+                        if (posicaoLeft < 1000) {
+                            posicaoLeft += 10;
+                            tiroElementClone.style.left = `${posicaoLeft}%`;
+                        } else {
+                            clearInterval(intervaloTiro);
+                            tiroElementClone.remove();
+                        }
+                    }, 50);
+    
+                    tiroElementClone.style.width = '20px';
+                    tiroElementClone.style.height = '20px';
+                }, i * 500);
+            }
+        }
+    }
+    
+    
+
+    
 
     static iniciarAnimacaoComerPlanta(gifElement) {
         console.log('comendo...');
@@ -305,3 +350,4 @@ class AnimacaoCartas {
 }
 
 export default AnimacaoCartas;
+
