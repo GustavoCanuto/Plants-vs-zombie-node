642ac4a1cc67d8b52ab6594e6de8c12a59f00c64
victorallves <viaugustoadr@gmail.com>
2024-01-19 16:35:48 -0300

    adicionando tiro nas plantas

--- public/assets/js/personagens.js
+++ public/assets/js/personagens.js
@@ -1,9 +1,10 @@
 export class Personagens {
-    constructor(imagePath, tempoRecarga, valorCard) {
+    constructor(imagePath, tempoRecarga, valorCard,vida) {
         this.imagePath = imagePath;
         this.tempoRecarga = tempoRecarga;
         this.recarregado = true;
         this.valorCard = valorCard;
+        this.vida = vida;
     }
 
     renderImage() {
@@ -14,22 +15,34 @@ export class Personagens {
         return imgElement;
     }
 
+    reduzirVida(dano) {
+        this.vida -= dano;
+        if (this.vida <= 0) {
+            this.morrer();
+        }
+    }
+
+    morrer() {
+        // Lógica para quando o personagem morrer
+        console.log(`${this.constructor.name} morreu.`);
+    }
+
 }
 
-const sunflower = new Personagens('../../assets/img/personagens/plants/sunflower.webp', 7500, 50);
-const peashooter = new Personagens('../../assets/img/personagens/plants/peashooter.webp', 20000, 100);
-const showpea = new Personagens('../../assets/img/personagens/plants/showpea.webp', 20000, 150);
-const repeater = new Personagens('../../assets/img/personagens/plants/repeater.webp', 20000, 150);
-const wallnut = new Personagens('../../assets/img/personagens/plants/wallnut.webp', 30000, 50);
-const cherrybomb = new Personagens('../../assets/img/personagens/plants/cherrybomb.webp',20000, 150);
-const potatomine = new Personagens('../../assets/img/personagens/plants/potatomine.webp',10000, 25);
-const cardtombstone = new Personagens('../../assets/img/personagens/zombies/cardtombstone.webp', 7, 50);
-const zombie = new Personagens('../../assets/img/personagens/zombies/zombie.webp', 7000, 25);
-const conehead = new Personagens('../../assets/img/personagens/zombies/conehead.webp', 10000, 75);
-const buckethead = new Personagens('../../assets/img/personagens/zombies/buckethead.webp', 10000, 100);
-const flagzombie = new Personagens('../../assets/img/personagens/zombies/flagzombie.webp', 30000, 300);
-const football = new Personagens('../../assets/img/personagens/zombies/football.webp', 10000, 150);
-const screendoor = new Personagens('../../assets/img/personagens/zombies/screendoor.webp', 5000, 100);
+const sunflower = new Personagens('./assets/img/personagens/plants/sunflower.webp', 7500, 50,5);
+const peashooter = new Personagens('./assets/img/personagens/plants/peashooter.webp', 20000, 100,5);
+const showpea = new Personagens('./assets/img/personagens/plants/showpea.webp', 20000, 150,5);
+const repeater = new Personagens('./assets/img/personagens/plants/repeater.webp', 20000, 150,5);
+const wallnut = new Personagens('./assets/img/personagens/plants/wallnut.webp', 30000, 50,5);
+const cherrybomb = new Personagens('./assets/img/personagens/plants/cherrybomb.webp',20000, 150,5);
+const potatomine = new Personagens('./assets/img/personagens/plants/potatomine.webp',10000, 25,5);
+const cardtombstone = new Personagens('./assets/img/personagens/zombies/cardtombstone.webp', 7500, 50,5);
+const zombie = new Personagens('./assets/img/personagens/zombies/zombie.webp', 7000, 25,5);
+const conehead = new Personagens('./assets/img/personagens/zombies/conehead.webp', 10000, 75,5);
+const buckethead = new Personagens('./assets/img/personagens/zombies/buckethead.webp', 10000, 100,5);
+const flagzombie = new Personagens('./assets/img/personagens/zombies/flagzombie.webp', 30000, 300,5);
+const football = new Personagens('./assets/img/personagens/zombies/football.webp', 10000, 150,5);
+const screendoor = new Personagens('./assets/img/personagens/zombies/screendoor.webp', 5000, 100,5);
 
 export const personagens = {
     sunflower: sunflower,
