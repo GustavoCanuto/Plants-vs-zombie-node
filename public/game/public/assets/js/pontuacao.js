20ea21dafafa160026a575f42a66406085802f8e
GustavoCanuto <gustavo-canuto@hotmail.com>
2024-01-20 19:12:36 -0300

    ajuste tamanho personagens

--- public/game/public/assets/js/pontuacao.js
+++ public/game/public/assets/js/pontuacao.js
@@ -2,7 +2,7 @@ import { personagens } from "./personagens.js";
 
 var pontoPlantas = document.getElementById("pontosPlanta");
 var pontoZombies = document.getElementById("pontosZombie");
-export var pontuacaoLado = [5000, 50];
+export var pontuacaoLado = [200, 5000];
 export var pontosLado = [pontoPlantas, pontoZombies]
 var board = document.querySelector('.board')
 
